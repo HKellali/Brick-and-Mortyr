14a4202bd773226d1b9dfaa86267f247f0145069
=HKellali <=>
2023-07-03 23:26:02 +0200

    added style for the charachter page

--- src/pages/Character/Character.tsx
+++ src/pages/Character/Character.tsx
@@ -1,34 +1,56 @@
 import "./Character.scss";
 import Status from "./Status";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { CharacterData } from "../../services/CharacterLoader";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const Character = () => {
   const data = useLoaderData();
   const character = data as CharacterData;
+  console.log(character);
 
   return (
     <div className="character">
+      <div className="label">Character Info</div>
       <div className="wrapper">
-        <div className="charachter-image">
+        <Link to="/">
+          <ArrowBackIcon></ArrowBackIcon>
+        </Link>
+        <div className="character-image">
           <img src={character.image} alt="" />
         </div>
         <div className="character-info">
           <table>
             <tbody>
               <tr>
-                <td>Name : {character.name}</td>
+                <th>Name</th>
+                <td className="first">{character.name}</td>
               </tr>
               <tr>
+                <th>Status</th>
                 <td>
-                  Status : <Status status={character.status}></Status>
+                  <Status status={character.status}></Status>
                 </td>
               </tr>
               <tr>
-                <td>Gender : {character.gender}</td>
+                <th>Gender</th>
+                <td>{character.gender}</td>
               </tr>
               <tr>
-                <td>Species : {character.species}</td>
+                <th>Species</th>
+                <td>{character.species}</td>
+              </tr>
+              <tr>
+                <th>Origin</th>
+                <td>{character.origin.name}</td>
+              </tr>
+              <tr>
+                <th>Location</th>
+                <td>{character.location.name}</td>
+              </tr>
+              <tr>
+                <th>Appeared in</th>
+                <td className="last">{character.episode.length} episodes</td>
               </tr>
             </tbody>
           </table>
